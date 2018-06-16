d070607ec43717679d73e8a9722bf9a801cdd071
guoyukunss <987226055@qq.com>
2018-06-16 08:51:39 +0800

    Update App.scala
    cuowu1

--- src/main/scala/com/guoyukun/App.scala
+++ src/main/scala/com/guoyukun/App.scala
@@ -11,7 +11,7 @@ object App extends Application {
 
   def main(args: Array[String]) {
     val rf = new RandomForestClassifier()
-      .setLabelCol("indexedLabel")jhvkjhbk
+      .setLabelCol("indexedLabel")jhvkjhbkewqer
       .setFeaturesCol("indexedFeatures")
       .setNumTrees(10)
   }
